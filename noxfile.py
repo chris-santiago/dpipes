1a18dbce5313991bfe810dc2618035a70c3dd90a
chris-santiago <cjsantiago@gatech.edu>
2023-04-25 21:08:09 -0400

    add pdm, nox

---
+++ noxfile.py
@@ -0,0 +1,23 @@
+import os
+
+import nox
+
+os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
+
+
+@nox.session(reuse_venv=True)
+def tests(session):
+    session.run("pdm", "install", "-dG", "test", external=True)
+    session.run("pytest")
+
+
+@nox.session(reuse_venv=True)
+def lint(session):
+    session.run("pdm", "install", "-dG", "qa", external=True)
+    session.run("ruff", "pipes")
+
+
+@nox.session(reuse_venv=True)
+def type_check(session):
+    session.run("pdm", "install", "-dG", "qa", external=True)
+    session.run("pyright", "pipes")
